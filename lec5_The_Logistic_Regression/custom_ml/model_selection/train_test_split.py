a8b4c997faf4ac0e67d4fe9718c79f471e07d370
Ziting Tang <ziting.tang@yahoo.com>
2022-12-04 06:52:33 -0600

    update

--- lec5_The_Logistic_Regression/custom_ml/model_selection/train_test_split.py
+++ lec5_The_Logistic_Regression/custom_ml/model_selection/train_test_split.py
@@ -6,7 +6,7 @@ __all__ = ["train_test_split"]
 def train_test_split(X, y, percent = .15):
     X, y = shuffle(X, y)
     split_index = np.int(np.round(X.shape[0])*.15)
-    X_train, y_train = X[:split_index], y[:split_index]
-    X_test, y_test = X[-split_index:], y[-split_index:]
+    X_train, y_train = X[:8], y[:8]
+    X_test, y_test = X[-8:], y[-8:]
 
     return X_train, y_train, X_test, y_test
